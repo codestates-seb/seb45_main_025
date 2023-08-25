19252a47a2d349fceb0c9315be8f422936476e84
yoongunyong <backendgun@gmail.com>
2023-08-25 14:20:50 +0900

    commit

---
+++ src/main/java/com/example/SSM/SsmApplication.java
@@ -0,0 +1,13 @@
+package com.example.SSM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmApplication.class, args);
+	}
+
+}
