c4af25c3e2526bbb27b96c440ebe39407aa03096
pyo8470 <rkswlska789@naver.com>
2023-08-25 16:46:18 +0900

    세팅

--- src/main/java/com/example/SSM/member/controller/MemberController.java
+++ src/main/java/com/example/SSM/member/controller/MemberController.java
@@ -1,4 +1,14 @@
 package com.example.SSM.member.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
 public class MemberController {
+    @GetMapping
+    public String Test(){
+        return "hi";
+    }
 }
