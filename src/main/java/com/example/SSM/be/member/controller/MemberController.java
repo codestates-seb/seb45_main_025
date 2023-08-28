12aa6296231239eb7dee524d35338688973f1d13
pyo8470 <rkswlska789@naver.com>
2023-08-28 16:06:51 +0900

    기본 세팅

--- src/main/java/com/example/SSM/be/member/controller/MemberController.java
+++ src/main/java/com/example/SSM/be/member/controller/MemberController.java
@@ -1,4 +1,7 @@
 package com.example.SSM.be.member.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MemberController {
 }
--- src/main/java/com/example/SSM/be/member/entity/member.java
+++ src/main/java/com/example/SSM/be/member/entity/member.java
@@ -1,11 +1,43 @@
 package com.example.SSM.be.member.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
 import java.util.Collection;
-
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class member implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private long userId;
+
+    @Column(length = 100, nullable = false,unique = true)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String username;
+
+    @Column(length = 300, nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String nick;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
