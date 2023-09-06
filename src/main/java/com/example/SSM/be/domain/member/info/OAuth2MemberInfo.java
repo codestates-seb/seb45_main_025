20805f13536199f4bae39e6214e58350823c8c49
pyo8470 <rkswlska789@naver.com>
2023-09-06 11:34:33 +0900

    feat. Member 필드 Nickname 추가, 로그인 성공 시, 헤더에 쿠키 정보 반환 추가

--- src/main/java/com/example/SSM/be/domain/member/info/OAuth2MemberInfo.java
+++
@@ -1,8 +0,0 @@
-package com.example.SSM.be.domain.member.info;
-
-public interface OAuth2MemberInfo {
-    String getProviderId(); //공급자 아이디 ex) google, facebook
-    String getProvider(); //공급자 ex) google, facebook
-    String getName(); //사용자 이름 ex) 홍길동
-    String getEmail(); //사용자 이메일 ex) gildong@gmail.com
-}
