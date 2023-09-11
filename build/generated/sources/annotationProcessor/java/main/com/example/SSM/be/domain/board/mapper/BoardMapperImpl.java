27787b3cc75de41188db12abf5d099552af69259
pyo8470 <rkswlska789@naver.com>
2023-09-11 09:32:09 +0900

    수정

--- build/generated/sources/annotationProcessor/java/main/com/example/SSM/be/domain/board/mapper/BoardMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/example/SSM/be/domain/board/mapper/BoardMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-07T16:00:32+0900",
+    date = "2023-09-11T09:27:49+0900",
     comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.0.jar, environment: Java 11.0.18 (Azul Systems, Inc.)"
 )
 @Component
