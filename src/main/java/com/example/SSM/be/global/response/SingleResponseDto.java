59548577461db31cbdfa5d81a29c2f3483ef6796
ParkYoungGil <75276860+ParkYoungGil@users.noreply.github.com>
2023-09-12 16:38:09 +0900

    Delete src directory

--- src/main/java/com/example/SSM/be/global/response/SingleResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.SSM.be.global.response;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-@Getter
-@AllArgsConstructor
-public class SingleResponseDto<T> {
-    private T data;
-}
