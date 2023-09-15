e660dbe65d42f0559af5811baf9df5383f1d9749
Juseong-Yu <aaa4591@naver.com>
2023-09-15 15:03:38 +0900

    oauth 수정

--- client/src/pages/LoadingOauth/LoadingOauth.js
+++ client/src/pages/LoadingOauth/LoadingOauth.js
@@ -1,21 +1,36 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-
 export default function OauthLoading(){
-  let pageparam = useLocation();
   const navigate = useNavigate();
-  let pageparam_split =  pageparam.split(/[?&=]/);
+  let pageparam = useLocation();
+  console.log(pageparam.search)
+  let pageparam_split =  pageparam.search.split(/[?&=]/);
   console.log(pageparam_split);
   let access_token = `Bearer ${pageparam_split[2].slice(9)}`;
   let refresh_token = `Bearer ${pageparam_split[4]}`;
+  console.log(access_token,refresh_token);
   localStorage.setItem('access_token',access_token);
   localStorage.setItem('refresh_token',refresh_token);
   if (pageparam_split[6] === 'true'){
-    navigate('/signup/oauth')
+    navigate('/signup/oauth');
   }else{
-    navigate('/')
+    navigate('/');
   }
-  useEffect(() => {
+  useEffect(()=>{
+    console.log(pageparam.search)
+    let pageparam_split =  pageparam.search.split(/[?&=]/);
+    console.log(pageparam_split);
+    let access_token = `Bearer ${pageparam_split[2].slice(9)}`;
+    let refresh_token = `Bearer ${pageparam_split[4]}`;
+    console.log(access_token,refresh_token);
+    localStorage.setItem('access_token',access_token);
+    localStorage.setItem('refresh_token',refresh_token);
+    if (pageparam_split[6] === 'true'){
+      navigate('/signup/oauth');
+    }else{
+      navigate('/');
+    }
   },[])
-  return (<></>)
+  
+  return (<div>hi</div>)
 }
