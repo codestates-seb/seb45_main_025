963857fdbbb8d0d5197aae7e35214b910a50b284
ParkYoungGil <eclipse9128@naver.com>
2023-09-17 17:33:28 +0900

    예외 수정

--- src/main/java/com/example/SSM/be/global/exception/ExceptionCode.java
+++ src/main/java/com/example/SSM/be/global/exception/ExceptionCode.java
@@ -6,9 +6,10 @@ public enum ExceptionCode {
 
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "회원이 존재합니다"),
+    MEMBER_IS_SLEEPING(404,"휴면 계정입니다"),
     MEMBER_IS_DELETED(404, "탈퇴한 회원입니다"),
     DIFFERENT_MEMBER(409, "회원님께서는 이 계정을 삭제할 권한이 없습니다"),
-    MEMBER_IS_SLEEPING(404,"휴면 계정입니다"),
+    NOT_MATCH_USER(404,"작성자가 아닙니다."),
     STATUS_NOT_LOGIN(404,"STATUS_NOT_LOGIN"),
     POST_NOT_FOUND(404, "Post not found"),
     COMMENT_NOT_FOUND(404, "Comment not found"),
@@ -20,8 +21,6 @@ public enum ExceptionCode {
     TOKEN_NOT_FOUND(404, "토큰을 찾을 수 없습니다"),
 
     IS_REFRESH_TOKEN(409,"리프레쉬 토큰입니다");
-
-
 
     @Getter
     private int status;
