23ad71e73bc46e1c9c23af19ea7d1dcfea4c97c2
minpppal <minpal203@gmail.com>
2023-10-27 11:01:30 +0900

    minEdit2

--- Front-End/client/src/pages/EditPage/EditPage.js
+++ Front-End/client/src/pages/EditPage/EditPage.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import Editor from '@toast-ui/editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
-import { useNavigate } from 'react-router-dom';
+import { useParams,useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import getAccessToken from '../../common/utils/getToken';
 import post5 from '../../common/image/post5.jpg';
@@ -22,6 +22,7 @@ function EditPage() {
     const [editor, setEditor] = useState(null);
     const [selectedImage] = useState(null);
     const navigate = useNavigate();
+    const { boardId } = useParams();
 
     const handleContentChange = () => {
         if (editor) {
@@ -51,21 +52,21 @@ function EditPage() {
         }
     };
 
-    const handlePublish = () => {
-        handleContentChange();
-        navigate('/CommunityList');
-    };
+    
 
     useEffect(() => {
         // 페이지가 로드될 때 글 데이터를 불러옴
         const fetchData = async () => {
             let access_token = getAccessToken();
             try {
-                const response = await axios.get(`${URI}/board/EDIT_BOARD_ID_HERE`, {
+                const response = await axios.get(`${URI}/board/${boardId}`,
+                {
                     headers: {
                         Authorization: access_token,
+                        
                     },
-                });
+                 }
+                );
                 setTitle(response.data.title);
                 const content = response.data.content;
 
@@ -81,6 +82,8 @@ function EditPage() {
                 });
                 editorInstance.setMarkdown(content);
                 setEditor(editorInstance);
+
+                
             } catch (error) {
                 console.error(error);
             }
@@ -89,6 +92,33 @@ function EditPage() {
         // handleContentChange, handlePublish 함수, 이미지 업로드 관련 코드 등 다른 부분은 그대로 유지
     }, []);
 
+    const editHandler = async () => {
+        let access_token = getAccessToken();
+    
+        try {
+            const formData = new FormData();
+            const content = response.data.content;
+            formData.append("title", title);
+            formData.append("content", content);
+            
+            const response = await axios.patch(`${URI}/board/${boardId}/update`, formData, {
+                headers: {
+                    Authorization: access_token,
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+    
+            // 응답 데이터 처리
+            console.log("백엔드 응답:", response.data);
+    
+            // 수정 완료 후 CommunityList 페이지로 이동
+            navigate('/CommunityList');
+        } catch (error) {
+            // 에러 처리
+            console.error("에러 발생:", error);
+            navigate('/CommunityList')
+        }
+    };
     return (
         <>
             <BackgroundImageContainer backgroundImage={`url(${post5})`}>
@@ -114,7 +144,7 @@ function EditPage() {
                     </EditorWrapper>
                 </EditorContainer>
                 <PublishButtonContainer>
-                    <PublishButton onClick={handlePublish}>Post</PublishButton>
+                    <PublishButton onClick={editHandler}>Post</PublishButton>
                 </PublishButtonContainer>
             </Container>
         </>
