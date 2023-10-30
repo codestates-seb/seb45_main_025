c507302d39552b0cb72af15ab0bf0402bd78409b
minpppal <minpal203@gmail.com>
2023-10-30 16:38:42 +0900

    minmin

--- Front-End/client/src/pages/WritePost/WritePost.js
+++ Front-End/client/src/pages/WritePost/WritePost.js
@@ -7,109 +7,103 @@ import getAccessToken from '../../common/utils/getToken';
 import post5 from '../../common/image/post5.jpg';
 
 import {
-    Container,
-    TitleInput,
-    BackgroundImageContainer,
-    EditorContainer,
-    EditorWrapper,
-    PublishButton,
-    PublishButtonContainer,
-    InputContainer
+  Container,
+  TitleInput,
+  BackgroundImageContainer,
+  EditorContainer,
+  EditorWrapper,
+  PublishButton,
+  PublishButtonContainer,
+  InputContainer
 } from './WritePost.styled';
 
 function WritePost() {
-    const URI = process.env.REACT_APP_API_URL;
-    const [title, setTitle] = useState('');
-    const [editor, setEditor] = useState(null);
-    const [selectedImage] = useState(null);
-    const navigate = useNavigate();
+  const URI = process.env.REACT_APP_API_URL;
+  const [title, setTitle] = useState('');
+  const [editor, setEditor] = useState(null);
+  const [selectedImage] = useState(null);
+  const navigate = useNavigate();
 
-    const handleContentChange = () => {
-        if (editor) {
-            const content = editor.getHTML();
-            const formData = new FormData();
-            formData.append('title', title);
-            formData.append('content', content);
+  const handleContentChange = () => {
+    if (editor) {
+      const content = editor.getHTML();
+      const formData = new FormData();
+      formData.append('title', title);
+      formData.append('content', content);
 
-            if (selectedImage) {
-                formData.append('image', selectedImage);
-            }
+      if (selectedImage) {
+        formData.append('image', selectedImage);
+      }
 
-            let access_token = getAccessToken();
+      let access_token = getAccessToken();
 
-            axios.post(`${URI}/board`, formData, {
-                headers: {
-                    Authorization: access_token,
-                    'Content-Type': 'multipart/form-data',
-                },
-            })
-                .then((response) => {
-                    console.log('백엔드 응답:', response);
-                })
-                .catch((error) => {
-                    console.error('에러 발생:', error);
-                });
-        }
-    };
-
-    const handlePublish = () => {
-        handleContentChange();
-        navigate('/CommunityList');
-    };
-
-
-
-    useEffect(() => {
-        const editorInstance = new Editor({
-            el: document.querySelector('#editor'),
-            height: '770px',
-            initialEditType: 'wysiwyg',
-            maxwidth: '800px',
-            overflow: 'ellipsis',
-            events: {
-                change: () => handleContentChange(),
-            },
+      axios
+        .post(`${URI}/board`, formData, {
+          headers: {
+            Authorization: access_token,
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+        .then((response) => {
+          console.log('백엔드 응답:', response);
+        })
+        .catch((error) => {
+          console.error('에러 발생:', error);
         });
+    }
+  };
 
-        setEditor(editorInstance);
-    }, []);
+  const handlePublish = () => {
+    handleContentChange();
+    navigate('/CommunityList');
+  };
 
-    return (
-        <>
-            <BackgroundImageContainer backgroundImage={`url(${post5})`}>
-                POST
-            </BackgroundImageContainer>
-            <Container>
-                <InputContainer>
-                    <TitleInput
-                        type="text"
-                        placeholder=" Title, 35 characters or less"
-                        value={title}
-                        onChange={(e) => {
-                            const inputValue = e.target.value;
-                            if (inputValue.length <= 35) {
-                                setTitle(inputValue);
-                            }
-                        }}
-                    />
-                </InputContainer>
-                <EditorContainer>
-                    <EditorWrapper>
-                        <div id="editor" style={{ overflowY: 'auto' }} />
+  useEffect(() => {
+    const editorInstance = new Editor({
+      el: document.querySelector('#editor'),
+      height: '770px',
+      initialEditType: 'wysiwyg',
+      maxwidth: '800px',
+      overflow: 'ellipsis',
+      events: {
+        change: () => handleContentChange()
+      }
+    });
 
-                    </EditorWrapper>
-                </EditorContainer>
+    setEditor(editorInstance);
+  }, []);
 
+  return (
+    <>
+      <BackgroundImageContainer backgroundImage={`url(${post5})`}>
+        POST
+      </BackgroundImageContainer>
+      <Container>
+        <InputContainer>
+          <TitleInput
+            type="text"
+            placeholder=" Title, 35 characters or less"
+            value={title}
+            onChange={(e) => {
+              const inputValue = e.target.value;
+              if (inputValue.length <= 35) {
+                setTitle(inputValue);
+              }
+            }}
+          />
+        </InputContainer>
+        <EditorContainer>
+          <EditorWrapper>
+            <div id="editor" style={{ overflowY: 'auto' }} />
+          </EditorWrapper>
+        </EditorContainer>
 
-
-
-
-                <PublishButtonContainer>
-                    <PublishButton onClick={handlePublish}>Post</PublishButton>
-                </PublishButtonContainer>
-            </Container>
-        </>
-    );
+        <PublishButtonContainer>
+          <PublishButton onClick={handlePublish}>Post</PublishButton>
+        </PublishButtonContainer>
+      </Container>
+    </>
+  );
 }
 
 export default WritePost;
