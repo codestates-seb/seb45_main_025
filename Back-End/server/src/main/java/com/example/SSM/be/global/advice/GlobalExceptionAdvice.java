8c0b4fff3d7ec9a911a115f7f1dc2f5d30c7099d
pyo8470 <84065357+pyo8470@users.noreply.github.com>
2024-01-24 21:33:59 +0900

    .

--- Back-End/server/src/main/java/com/example/SSM/be/global/advice/GlobalExceptionAdvice.java
+++ Back-End/server/src/main/java/com/example/SSM/be/global/advice/GlobalExceptionAdvice.java
@@ -80,8 +80,6 @@ public class GlobalExceptionAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(Exception e) {
         log.error("# handle Exception", e);
-        // TODO 애플리케이션의 에러는 에러 로그를 로그에 기록하고, 관리자에게 이메일이나 카카오 톡,
-        //  슬랙 등으로 알려주는 로직이 있는게 좋습니다.
 
         final ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
 
